c216b7ecb5ed06ea20ed7fe4aa95c3ddbbefa3bc
Aziza Solace Afadhali <s.afadhali@alustudent.com>
2026-09-25 12:08:12 +0200

    feat: add ThemeProvider and useTheme hook for theme management

--- react-state-management/src/context/ThemeContext.tsx
+++ react-state-management/src/context/ThemeContext.tsx
@@ -8,3 +8,25 @@ interface ThemeContextType{
     theme:Theme;
     toggleTheme:()=>void;
 }
+
+const ThemeContext = createContext<ThemeContextType | undefined>(undefined)
+
+export const ThemeProvider=({children}:{children:ReactNode})=>{
+   const [theme,setTheme] = useState<Theme>(LIGHT_THEME)
+   
+   const toggleTheme=()=>{
+    setTheme((prevTheme) =>(prevTheme===LIGHT_THEME?DARK_THEME:LIGHT_THEME));
+   };
+   return (
+    <ThemeContext.Provider value={{theme,toggleTheme}}>
+        {children}
+    </ThemeContext.Provider>
+   )
+}
+export const useTheme = ()=>{
+    const context = useContext(ThemeContext);
+    if (!context){
+        throw new Error('useTheme must be used within a ThemeProvider');
+    }
+    return context;
+};
