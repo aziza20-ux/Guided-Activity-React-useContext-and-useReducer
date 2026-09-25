8e64a437e4e4934986397d032fd556866726ee0f
Aziza Solace Afadhali <s.afadhali@alustudent.com>
2026-09-25 13:33:34 +0200

    feat: refactor App and main components for improved structure and add taskReducer for state management

--- react-state-management/src/App.tsx
+++ react-state-management/src/App.tsx
@@ -8,7 +8,7 @@ import { useTheme } from './context/ThemeContext';
 function App(){
    const {theme}=useTheme();
   return(
-    <div style={{backgroundColor:theme===LIGHT_THEME?'#FFFFFF':'#242629',color:theme===LIGHT_THEME?'#000000':'#FFFFFF',Button:theme===LIGHT_THEME?'#1E90FF':'#85D1B0'}}>
+    <div style={{backgroundColor:theme===LIGHT_THEME?'#FFFFFF':'#242629',color:theme===LIGHT_THEME?'#000000':'#FFFFFF'}}>
     <NavBar />
     </div>
   )
--- react-state-management/src/main.tsx
+++ react-state-management/src/main.tsx
@@ -5,9 +5,9 @@ import App from './App.tsx'
 import {ThemeProvider} from './context/ThemeContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <ThemeProvider>
-    <StrictMode>
-      <App />
-    </StrictMode>
-  </ThemeProvider>
+   <StrictMode>
+    <ThemeProvider>
+        <App />
+    </ThemeProvider>
+   </StrictMode>
 )
