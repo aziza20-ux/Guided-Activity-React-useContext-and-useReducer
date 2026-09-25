6bd2a4b51bf95771235e906b56c419a16c03a0e9
Aziza Solace Afadhali <s.afadhali@alustudent.com>
2026-09-25 12:49:07 +0200

    feat: refactor App component to use NavBar and add ThemeProvider

--- react-state-management/src/components/Navbar.tsx
+++ react-state-management/src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useTheme } from '../context/ThemeContext';
-import styles from './Navbar.module.css';
+import styles from '../styles/Navbar.module.css';
 import { DARK_THEME, LIGHT_THEME } from '../constants/theme';
 
 
